11f38e2064c3acec963b3e8ab735517bcd2b7da2
Kristen <kristen3119@gmail.com>
2017-08-07 15:50:47 -0400

    updating app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-//var request = require("request");
+
+
 var yargs = require("yargs");
 var geocode = require("./geocode/geocode.js")
+var weather = require("./weather/weather.js")
 const argv = yargs.options({
     a: {
     	demand:true,
@@ -13,16 +15,25 @@ const argv = yargs.options({
 .alias("help", "h")
 .argv;
 
+
 geocode.geocodeAddress(argv, function callback(errorMessage, results){
     if(errorMessage){console.log(errorMessage)}
 
     else{
-        console.log(JSON.stringify(results, undefined, 2))
-    }
+        console.log(results.address)
+
+weather.getWeather(results.latitude, results.longitude, function callback(errorMessage, weatherResult){
+  if(errorMessage){ console.log(errorMessage)}
+    else{console.log("It's currently "+weatherResult.temperature+ " but it feelse like " + weatherResult.apparentTemperature)}
 });
 
+    }
+});     
+
+//weather.getWeather(33.9691145, -84.149745, function callback(errorMessage, result){});
 
 
+//101c1b9777bb28feaaea270678016335
 
 
-//continue on lesson 34
+//continue on lesson 36
